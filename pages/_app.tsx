146f309b6ff03782909a667d6f140852768c7c46
MatyAlfonso <matt1998boca@gmail.com>
2022-03-12 16:30:31 +0100

    First commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import '../styles/globals.css';
+
+import { lightTheme, darkTheme } from '../themes';
+import { UIProvider } from '../context/ui';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+  return (
+    <UIProvider>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </UIProvider>
+  )
+};
 
-export default MyApp
+export default MyApp;
